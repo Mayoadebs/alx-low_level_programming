9648a0faec54b41fffa8677412c4c7d8ef285c9f
Mayoadebs <mawowa362@gmail.com>
2022-09-15 15:59:52 -0700

    Task done

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -23,6 +23,35 @@ int main(void)
 }
 /**
  * isPrime - analise if a number is prime or not
+ *  @n: number to check
+ *  Return: true if it is prime false if not
+ */
+int isPrime(long int n)
+{
+	int i;
+
+	if (n <= 1)
+	{
+		return (0);
+	}
+	else if (n == 2)
+	{
+		return (1);
+	}
+	else
+	{
+		for (i = 2; i < n; i++)
+		{
+			if (n % i == 0)
+			{
+				return (0);
+			}
+		}
+	return (1);
+	}
+}
+/**
+ * biggestFactor - returns the biggest prime factor of a number
  * @a: number to check
  * Return: biggest factor
  */
